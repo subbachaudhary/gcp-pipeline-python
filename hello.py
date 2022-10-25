2d7e9b53a22a098b0bc01947a9a54e9f146ba8d1
Your Name <you@example.com>
2022-10-25 15:26:55 +0545

    add new version of app

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask:v1.0 test subba!!'
+    return 'Web App with Python Flask:v1.0 test subba chaudhary!!'
 
 app.run(host='0.0.0.0', port=8080)
